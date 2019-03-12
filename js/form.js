96c8f22004a8dc99385b280d53d813be2aa6c45c
Павел <hereff@ya.ru>
2019-03-12 22:59:49 +0300

    Готовимся к защите. Попытка 2

--- js/form.js
+++ js/form.js
@@ -1,5 +1,6 @@
 var link = document.querySelector(".lead-magnet-intro a");
 var hide = document.querySelector(".hotel-search-index");
+hide.classList.add("hide");
 link.addEventListener("click", function (evt) {
 evt.preventDefault();
 hide.classList.toggle("hide");
